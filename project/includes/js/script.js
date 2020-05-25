470830e77eed48589d80ec88d0cff34266216685
idan kario <59645247+idankario@users.noreply.github.com>
2020-05-25 07:02:23 +0300

    Add files via upload

---
+++ project/includes/js/script.js
@@ -0,0 +1,44 @@
+/*var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}*/
+
+
+
+
+
+
+
+/*datepicker function*/
+ $('#sandbox-container .input-group.date').datepicker({
+startDate: "05/25/2020",
+daysOfWeekDisabled: "5,6",
+startView: 1,
+clearBtn: true,
+calendarWeeks: true,
+autoclose: true,
+todayHighlight: true
+
+});
